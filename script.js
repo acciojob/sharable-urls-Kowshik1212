36ad270632ee07a3a57e0010fa854bdbae3d9c0a
Kowshik1212 <80085726+Kowshik1212@users.noreply.github.com>
2023-09-07 20:13:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // your code here
+const nameEl=document.querySelector("#name");
+const yearEl=document.querySelector("#year");
+const submitEl=document.querySelector(".submit");
+const header=document.querySelector(".link");
+
+submitEl.addEventListener('click',function(){
+	let name=nameEl.value;
+	let year=yearEl.value;
+	let stringToBeAppended="https://localhost:8080/";
+	if(name || year)
+	{
+		stringToBeAppended+="?"
+	}
+	if(name)
+	{
+		stringToBeAppended+="name="+name;
+	}
+	if(name && year)
+	{
+		stringToBeAppended+="&"
+	}
+	if(year)
+	{
+		stringToBeAppended+="year="+year;
+	}
+	header.innerHTML= stringToBeAppended;
+})
